999d4e81e45bd1cfcb598df76177196bcf29fcc2
Kevinloritsch <56408094+Kevinloritsch@users.noreply.github.com>
2020-02-23 20:23:11 -0800

    I did something!!!
    Cube Text 1.0

---
+++ Learning How to Use Unity/Assets/Code/EndTrigger.cs
@@ -0,0 +1,12 @@
+﻿
+using UnityEngine;
+
+public class EndTrigger : MonoBehaviour
+{
+    public Management gameManager;
+
+    void OnTriggerEnter ()
+    {
+        gameManager.CompleteLevel();
+    }
+}
---
+++ Learning How to Use Unity/Assets/Code/PlayerCollision.cs
@@ -0,0 +1,15 @@
+﻿
+using UnityEngine;
+
+public class PlayerCollision : MonoBehaviour
+{
+    public PlayerMovement movement;
+    void OnCollisionEnter(Collision collisionInfo)
+    {
+        if (collisionInfo.collider.tag == "Obstacles")
+        {
+            movement.enabled = false;
+            FindObjectOfType<Management>().EndGame();
+        }
+    }
+}
